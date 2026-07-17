cbb683ffad37b4ad45564cc6482b179386cc5fbc
AyushGhagre-09 <ghagareayush2001@gmail.com>
2026-07-17 07:51:32 +0530

    another approach

--- Easy/06_game_player_analysis_i.sql
+++ Easy/06_game_player_analysis_i.sql
@@ -8,3 +8,18 @@ player_id,
 MIN(event_date) AS first_login 
 FROM Activity
 GROUP BY player_id;
+
+
+-- SELECT 
+--  T.player_id,
+--  T.first_login
+-- FROM (
+--     SELECT
+--     player_id,
+--     event_date AS first_login,
+--     ROW_NUMBER()OVER(
+--         PARTITION BY player_id
+--         ORDER BY event_date ASC
+--     ) AS  rn
+--     FROM Activity) AS T
+-- WHERE T.rn=1;
