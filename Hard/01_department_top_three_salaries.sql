df37c60e6207dcae905fc3dd3d7779b5d45f0d53
AyushGhagre-09 <ghagareayush2001@gmail.com>
2026-07-20 18:45:18 +0530

    20-07-2026

---
+++ Hard/01_department_top_three_salaries.sql
@@ -0,0 +1,26 @@
+-- Problem  :  Department Top Three Salaries
+-- Difficulty: Hard
+-- Link     :  https://leetcode.com/problems/department-top-three-salaries/description/
+
+
+-- Your solution here
+SELECT
+ T.Department,
+ T.Employee,
+ T.Salary
+FROM 
+    (SELECT
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary,
+    DENSE_Rank()OVER(
+        PARTITION BY d.name
+        ORDER BY e.salary DESC
+        ) As rn
+    FROM 
+    Employee AS e
+    INNER JOIN Department AS d
+    ON e.departmentId=d.id) AS T
+WHERE T.rn<=3;
+
+
