a2d982b31799459f7ce7d513e7fccff3a0fa81dc
AyushGhagre-09 <ghagareayush2001@gmail.com>
2026-08-03 08:06:28 +0530

    03-08-2026

---
+++ Medium/13_monthly_transactions_i.sql
@@ -0,0 +1,15 @@
+-- Problem  :    Monthly Transactions I
+-- Difficulty: Medium 
+-- Link     : https://leetcode.com/problems/monthly-transactions-i/description/
+
+-- Your solution here
+SELECT
+DATE_FORMAT(trans_date, '%Y-%m') AS 'month',
+country,
+COUNT(trans_date) AS trans_count,
+COUNT(CASE WHEN state="approved" THEN 1 END ) AS approved_count,
+SUM(amount) AS trans_total_amount,
+COALESCE(SUM(CASE WHEN state="approved" THEN amount END ),0) AS approved_total_amount
+FROM 
+Transactions
+GROUP BY month,country;
