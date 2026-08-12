9fd4d7d1fa0d9381190631f5251c1436f8ceb335
AyushGhagre-09 <ghagareayush2001@gmail.com>
2026-08-12 08:58:12 +0530

    12-08-2026

---
+++ Easy/27_the_number_of_employees_which_report_to_each_employee.sql
@@ -0,0 +1,22 @@
+-- Problem  :    The Number of Employees Which Report to Each Employee
+-- Difficulty: Easy 
+-- Link     :  https://leetcode.com/problems/the-number-of-employees-which-report-to-each-employee/description/?envType=study-plan-v2&envId=top-sql-50
+
+-- Your solution here
+
+SELECT
+  T.report_to AS employee_id,
+  T.name,
+  COUNT(T.employee_id) AS reports_count,
+  ROUND((AVG(T.age)) )AS average_age
+FROM(SELECT
+ e.employee_id,
+ r.employee_id AS report_to,
+ r.name AS name,
+ e.age
+FROM Employees AS e
+INNER JOIN Employees AS r
+  ON e.reports_to=r.employee_id
+) AS T
+GROUP BY T.report_to,T.name
+ORDER BY employee_id ASC;
