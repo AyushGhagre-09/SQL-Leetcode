f8ebae1cb660973ac38b74b1d485610a29018b7d
Ayush Ghagre <147733805+AyushGhagre-09@users.noreply.github.com>
2026-08-24 20:10:10 +0530

    24-08-2026

---
+++ Easy/36_employees_whose_manager_left_the_company.sql
@@ -0,0 +1,16 @@
+-- Problem  :  Employees Whose Manager Left the Company
+-- Difficulty: Easy 
+-- Link     : https://leetcode.com/problems/employees-whose-manager-left-the-company/description/?envType=study-plan-v2&envId=top-sql-50
+
+-- Your solution here
+SELECT 
+e.employee_id
+FROM Employees AS e 
+LEFT JOIN Employees AS m
+ ON e.manager_id=m.employee_id
+WHERE e.salary<30000  
+  AND e.manager_id IS NOT NULL
+  AND m.employee_id IS NULL
+ORDER BY e.employee_id
+
+--  AND m.employee_id IN (SELECT employee_id FROM Employees);
