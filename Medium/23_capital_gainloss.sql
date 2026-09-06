1cb51b80417c2775837bfc81bab075840d50b7fa
Ayush Ghagre <147733805+AyushGhagre-09@users.noreply.github.com>
2026-09-06 17:05:52 +0530

    06-09-2026

---
+++ Medium/23_capital_gainloss.sql
@@ -0,0 +1,10 @@
+-- Problem  :  Second Highest Salary
+-- Difficulty: Medium 
+-- Link     : https://leetcode.com/problems/capital-gainloss/description/
+
+-- Your solution here
+SELECT 
+stock_name,
+SUM(CASE WHEN operation='Sell' THEN price END)-SUM(CASE WHEN operation='Buy' THEN price END) AS capital_gain_loss
+FROM  Stocks
+GROUP BY stock_name;
