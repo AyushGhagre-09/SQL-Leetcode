9e25236d7528685e037db7794b1983371e82b05f
Ayush Ghagre <147733805+AyushGhagre-09@users.noreply.github.com>
2026-09-07 09:51:43 +0530

    07-09-2026

---
+++ Medium/24_odd_and_even_transactions.sql
@@ -0,0 +1,12 @@
+-- Problem  :   Odd and Even Transactions
+-- Difficulty: Medium 
+-- Link     : https://leetcode.com/problems/odd-and-even-transactions/description/
+
+-- Your solution here
+SELECT
+ transaction_date,
+ SUM(CASE WHEN amount%2!=0 THEN amount ELSE 0 END )AS odd_sum,
+ SUM(CASE WHEN amount%2=0 THEN amount ELSE 0 END )AS even_sum
+FROM transactions
+GROUP BY transaction_date
+ORDER BY transaction_date ASC;
